feaf65a7c7e94bcc792fb043675f4fe59d69aea9
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-11-30 23:02:46 +0530

    Create minimum number of ones .cpp
    added starter 67 solution

---
+++ minimum number of ones .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int anshii;
+	cin>>anshii;
+	while(anshii--)
+	{
+	    int n;
+	    cin>>n;
+	    cout<<n/2<<endl;
+	}
+	
+	return 0;
+}
