6e7688bf29850d69b1993edc2c256a945a5d01d0
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-31 09:13:07 +0530

    Create Credit Score.cpp
    added a solution

---
+++ Credit Score.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if(x>=720){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+ return 0;
+}
