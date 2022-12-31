bcab4b505119b7740bb83ec190d77b6d09613440
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-31 11:27:24 +0530

    Create Nearest Exit.cpp
    added a codechef solution

---
+++ Nearest Exit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X<=50)
+	    cout<<"LEFT"<<endl;
+	    else
+	    cout<<"RIGHT"<<endl;
+	}
+	return 0;
+}
