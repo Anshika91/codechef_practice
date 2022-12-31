b31660a168c2c498f731842b2962d2aec2008419
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-31 10:52:09 +0530

    Create The Three Topics.cpp
    added a codechef solution

---
+++ The Three Topics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	if(a==x || b==x || c==x)
+	cout<<"YES"<<endl;
+	else
+	cout<<"NO"<<endl;
+	return 0;
+}
