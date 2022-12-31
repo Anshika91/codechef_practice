c466855336802365fd485402ce07f190cecb53a8
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2022-12-31 11:24:05 +0530

    Create Air Conditioner Temperature.cpp
    added a codechef solution

---
+++ Air Conditioner Temperature.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(b>=a && c<=b) 
+	    cout<<"YES"<<endl;
+     	else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
