2954c5f8386b09491fdc19f871ddddb11438cfce
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-01-01 19:29:29 +0530

    Create Increase IQ.cpp
    added a codechef solution

---
+++ Increase IQ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	    int x;
+	    cin>>x;
+	    if(x+7>170)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	return 0;
+}
