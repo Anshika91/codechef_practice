331ae904e6d9200e26b78be1fe9bbd0e8ac83f60
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-01-29 19:04:02 +0530

    Create Alterig work days.cpp
    added a codechef solution

---
+++ Alterig work days.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,p,q;
+        cin>>a>>b>>p>>q;
+        if(p%a==0 && q%b==0){
+            if(abs(p/a - q/b) <= 1){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
+
+//hello this is aj
