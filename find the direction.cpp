c0f2f38333375c85bfe3d4142bcc012e43da131c
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-02-01 19:23:47 +0530

    Create find the direction.cpp
    added a codechef solution

---
+++ find the direction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x; cin>>x;
+	    if(x%4==0) cout<<"North\n";
+	    else if(x%4==1) cout<<"East\n";
+	    else if(x%4==2) cout<<"South\n";
+	    else if(x%4==3) cout<<"West\n";
+	    
+	}
+	return 0;
+}
