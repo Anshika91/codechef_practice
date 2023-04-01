c1043b72ddbe1af0a07b3d5593bd8a8c9d00f664
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-04-01 19:58:32 +0530

    Create it is a vowel or consonant.cpp
    added a codechef solution

---
+++ it is a vowel or consonant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    cin>>a;
+    if((a=='A') || (a=='E') || (a=='I') || (a=='O') || (a=='U'))
+    cout<<"Vowel\n";
+    else
+    cout<<"Consonant\n";
+	return 0;
+}
