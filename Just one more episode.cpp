69ed685f2b397527103a1e2ca4fbc7f6e8785534
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-04-11 19:44:31 +0530

    Create Just one more episode.cpp
    added a codechef solution

---
+++ Just one more episode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n>24)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"No"<<endl;
+	}
+	return 0;
+}
