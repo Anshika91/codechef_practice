de71fbb5cd10007fb2e6d75838021cc6c18efc34
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-04-30 09:05:09 +0530

    Create Making a Meal.cpp
    added a codechef solution

---
+++ Making a Meal.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	    int n;
+	    cin >> n;
+	    unordered_map<char, int> um;
+	    for (int i = 0; i < n; ++i) {
+	        string s;
+	        cin >> s;
+	        for (char j: s) {
+	            um[j]++;
+	        }
+	    }
+	    cout << min({um['c'] / 2, um['o'], um['d'], um['e'] / 2, um['h'], um['f']}) << endl;
+	}
+	return 0;
+}
