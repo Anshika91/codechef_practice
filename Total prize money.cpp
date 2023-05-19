ff5ef9e24752301d915cff47a95eef07b3c6266f
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-05-19 21:46:44 +0530

    Create Total prize money.cpp
    added a codechef solution

---
+++ Total prize money.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    cout<<(X*10)+(Y*90)<<endl;
+	}
+	return 0;
+}
