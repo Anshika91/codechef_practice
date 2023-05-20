a2e05511221d4573296de224170d0faaa192f855
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-05-20 16:36:25 +0530

    Create Audiable range.cpp
    added a codechef solution

---
+++ Audiable range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>=67&&x<=45000)
+	    cout<<"yes"<<endl;
+	    else 
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
