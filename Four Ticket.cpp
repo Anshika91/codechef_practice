533f87050d566c51fe30cea2942adde6bbed003a
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-05-21 20:49:42 +0530

    Create Four Ticket.cpp
    added a codechef solution

---
+++ Four Ticket.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n;
+	    cin>>n;
+	    if(n*4<=1000)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
