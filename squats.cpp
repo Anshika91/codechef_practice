ff2dd6df1e45dfe7deee0ddd833e5a7e02255bb4
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-07-03 18:50:01 +0530

    Create squats.cpp
    updated

---
+++ squats.cpp
@@ -0,0 +1,21 @@
+#include<iostream> #include<math.h>
+#include<string.h> #include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(ll s)
+{
+    cout << 15*s << endl;
+}
+int main()
+{
+    ll t; cin>>t;
+    for(ll i=0;i<t;i++)
+    {
+        ll x; cin>>x;
+        solve(x);
+    }
+    
+    return 0;
+    
+}
